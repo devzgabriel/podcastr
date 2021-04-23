44f3f93e70c07d0f4e89cc2b3585b06f258b84b5
Gabriel Silva <devzgabriel@gmail.com>
2021-04-23 09:08:53 -0300

    static generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,20 @@
-import { Header } from '../components/Header'
+export default function Home(props) {
+  return (
+    <>
+      <h1>Index</h1>
+      <p>props.episodes</p>
+    </>
+  )
+}
 
-export default function Home() {
-  return <h1>Index</h1>
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
